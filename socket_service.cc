e933e05d3f7ae30c972a70a2117c1c5365d0087f
csq <csq@csq-VirtualBox.(none)>
2014-03-28 01:49:44 -0700

    improve tcp server

--- socket_service.cc
+++ socket_service.cc
@@ -1,30 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
+#include <sys/wait.h>
 #include <sys/socket.h>
 #include <strings.h>
 #include <unistd.h>
 #include <netinet/in.h>
-
+#define MAX_BUF 8192
 void doprocessing (int sock)
 {
     int n;
-    char buffer[256];
-
-    bzero(buffer,256);
-
-    n = read(sock,buffer,255);
-    if (n < 0)
+    char buffer[MAX_BUF];
+    int i = 0, index = 0;
+    bzero(buffer,MAX_BUF);
+    n = write(sock,"Hello!\n",7);
+    while(1)
     {
-        perror("ERROR reading from socket");
-        exit(1);
-    }
-    printf("Here is the message: %s\n",buffer);
-    n = write(sock,"I got your message",18);
-    if (n < 0)
-    {
-        perror("ERROR writing to socket");
-        exit(1);
+        n = read(sock,buffer,MAX_BUF-1-index);
+        index += n;
+        if(index >=MAX_BUF-1)
+        {
+            index = 0;
+        }
+        if (n < 0)
+        {
+            perror("ERROR reading from socket");
+            exit(1);
+        }
+        for(i=0;i<index;i++)
+        {
+            if(buffer[i] == '\n')
+            {
+                printf("Here is the message\n");
+                printf("%*.*s\n", 0, i, buffer);
+                n = write(sock,"\n",1);
+                if (n < 0)
+                {
+                    perror("ERROR writing to socket");
+                    exit(1);
+                }
+                index = 0;
+                break;
+            }
+        }
+
     }
 }
 int main( int argc, char *argv[] )
@@ -35,7 +54,9 @@ int main( int argc, char *argv[] )
     struct sockaddr_in serv_addr, cli_addr;
     int  n;
     int pid = -1;
-
+    int current_connection = 0;
+    pid_t result = 0;
+    int status =0;
     /* First call to socket() function */
     sockfd = socket(AF_INET, SOCK_STREAM, 0);
     if (sockfd < 0)
@@ -72,6 +93,20 @@ int main( int argc, char *argv[] )
             perror("ERROR on accept");
             exit(1);
         }
+        if(current_connection > 0)
+        {
+            result = waitpid(pid, &status, WNOHANG);
+            if(result == 0)
+            {
+                write(newsockfd,"BUSY\n",4);
+                close(newsockfd);
+                continue;
+            }
+            else
+            {
+                current_connection --;
+            }
+        }
         /* Create child process */
         pid = fork();
         if (pid < 0)
@@ -89,6 +124,7 @@ int main( int argc, char *argv[] )
         }
         else
         {
+            current_connection ++;
             close(newsockfd);
         }
     } /* end of while */
